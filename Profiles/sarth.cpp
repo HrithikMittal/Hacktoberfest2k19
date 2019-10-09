aec9b7aad4ad2307a9fb5ff2141d584a13c7532a
Restyled.io <commits@restyled.io>
2019-10-09 15:04:04 +0000

    Restyled by clang-format

--- Profiles/sarth.cpp
+++ Profiles/sarth.cpp
@@ -1,9 +1,7 @@
-#include<iostream.h>
-int main()
-{
-    int i,n=5;
-    for(i=0; i<n; i++)
-    {
-        cout<<i;
-    }
+#include <iostream.h>
+int main() {
+  int i, n = 5;
+  for (i = 0; i < n; i++) {
+    cout << i;
+  }
 }
