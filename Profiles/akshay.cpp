e91eb0a7c6b274cf2197389c84ae0915e4045497
Restyled.io <commits@restyled.io>
2019-10-11 07:19:02 +0000

    Restyled by clang-format

--- Profiles/akshay.cpp
+++ Profiles/akshay.cpp
@@ -1,54 +1,48 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int bubble_sort_swap(int array[],int size) {
+int bubble_sort_swap(int array[], int size) {
 
-    int swap =0;
-    for(int i = size-2; i>=0; i--) {
-        for(int j=0; j<=i; j++) {
+  int swap = 0;
+  for (int i = size - 2; i >= 0; i--) {
+    for (int j = 0; j <= i; j++) {
 
-            if(array[j] >array[j+1]) {
+      if (array[j] > array[j + 1]) {
 
-                swap++;
-                // cout<<"Swap:"<<swap<<endl;
-                int temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
-            }
-        }
+        swap++;
+        // cout<<"Swap:"<<swap<<endl;
+        int temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
     }
+  }
 
-    return swap;
+  return swap;
 }
 
-
-
 int main() {
-    int tc;
-    scanf("%d",&tc);
+  int tc;
+  scanf("%d", &tc);
 
-    int size;
+  int size;
 
-    int answer;
+  int answer;
 
-    for(int test=0; test< (tc); test++) {
-        scanf("%d",&size);
-        int array[size];
-        for(int i=0; i<size; i++) {
-            scanf("%d",&array[i]);
-        }
-
-        // for(int i=0;i<size;i++){
-        //     printf("%d",array[i]);
-        // }
-
-        answer = bubble_sort_swap(array,size);
-        printf("%d \n",answer);
-
+  for (int test = 0; test < (tc); test++) {
+    scanf("%d", &size);
+    int array[size];
+    for (int i = 0; i < size; i++) {
+      scanf("%d", &array[i]);
     }
 
+    // for(int i=0;i<size;i++){
+    //     printf("%d",array[i]);
+    // }
 
+    answer = bubble_sort_swap(array, size);
+    printf("%d \n", answer);
+  }
 
-
-    return 0;
+  return 0;
 }
