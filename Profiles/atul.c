f2f4cfdaea21c77b29a3a6d793a6b3d5581a4098
atul0110 <56165943+atul0110@users.noreply.github.com>
2019-10-13 10:52:54 +0530

    updated file

---
+++ Profiles/atul.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter 1at num");
+    scanf("%d",&a);
+    printf("enter 2nd num");
+    scanf("%d",&b);
+    c=a+b;
+    printf("sum is %d",c);
+    return 0;
+}
